e63e8672aa88253c2ad40895b868bd75a812a85b
modestojj <jeanfmodesto6734@gmail.com>
2025-09-18 23:52:31 +0000

    formato X

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,27 +4,24 @@
 #define coluna 9
 
 int main(){
-    int matriz[linha][coluna] = {0};
-    int meio = coluna / 2;
+int matriz[linha][coluna] = {0};
+int meio = coluna / 2;
 
 
-    for (int i = 0; i < linha; i++) {
-       int inicio = meio - i + 1 ;
-       int fim = meio + i ;
+        
+ 
+    //formato X
+     for(int i = 0;i < linha; i++){
+        for(int j = 0; j < coluna; j++){
+            if(i == j || i + j == coluna - 1){//i == j desenha a diagonal principal
+                matriz[i][j] = 3;//i + j == coluna - 1 desenha a diagonal secundária
+            }
+        }
+    }
 
-    // garante que não ultrapasse os limites
-        if( inicio < 0) inicio = 0;
-       if (fim > coluna) fim = coluna - 10 ;
-
-       for (int j = inicio; j < fim; j++){
-        matriz[i][j] = 3;
-       }
-    }   
-
-         
-    for (int i = 0; i < linha; i++){  
-        for (int j = 0; j < coluna; j++){
-            printf("%d ", matriz[i][j] );
+    for(int i = 0; i < linha; i++){
+        for(int j = 0; j < coluna; j++){
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     } 
