303cec90498b289258f402166de71b2121622af6
modestojj <jeanfmodesto6734@gmail.com>
2025-09-18 23:41:36 +0000

    Batalha naval com navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,8 +4,9 @@
 #define colunaNaval 11
 
 int main(){
+    //aqui aparece as letras em cima de A a J
     char linha[10] = {'A', 'B','C','D',
-'E', 'F', 'G', 'H', 'I', 'J'}; 
+    'E', 'F', 'G', 'H', 'I', 'J'}; 
     int matriz[linhaNaval][colunaNaval];
     for (int i = 0; i <= 9; i++)
     {
@@ -16,7 +17,7 @@ int main(){
     printf("\n");
 //==================================================
 
-    
+    //logica para numerar de 1 a 10 com os 0 ao lado com 4 casas de navios na diagonal 
     for (int i = 1; i < linhaNaval; i++){  
         for (int j = 1; j < colunaNaval; j++){
         matriz[i][j] = 0;
