48ba512419428427ad3ec61702c0c1d5466613d2
modestojj <jeanfmodesto6734@gmail.com>
2025-09-18 23:48:28 +0000

    formato triangulo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 
-#define linhaNaval 11
-#define colunaNaval 11
+#define linha 9
+#define coluna 9
 
 int main(){
-    //aqui aparece as letras em cima de A a J
-    char linha[10] = {'A', 'B','C','D',
-    'E', 'F', 'G', 'H', 'I', 'J'}; 
-    int matriz[linhaNaval][colunaNaval];
-    for (int i = 0; i <= 9; i++)
-    {
-        printf("  ");
-        printf("   %c", linha[i]);  
-        
-    }
-    printf("\n");
-//==================================================
+    int matriz[linha][coluna] = {0};
+    int meio = coluna / 2;
 
-    //logica para numerar de 1 a 10 com os 0 ao lado com 4 casas de navios na diagonal 
-    for (int i = 1; i < linhaNaval; i++){  
-        for (int j = 1; j < colunaNaval; j++){
-        matriz[i][j] = 0;
-        }
-    }
 
-    for(int i  = 1, j = 10; i <= 4; i++, j--){ //i linha 1 e J coluna 10 com --
+    for (int i = 0; i < linha; i++) {
+       int inicio = meio - i + 1 ;
+       int fim = meio + i ;
+
+    // garante que não ultrapasse os limites
+        if( inicio < 0) inicio = 0;
+       if (fim > coluna) fim = coluna - 10 ;
+
+       for (int j = inicio; j < fim; j++){
         matriz[i][j] = 3;
-        
-    }
+       }
+    }   
 
-    for (int i = 1; i < linhaNaval; i++){
-        printf("%2d ", i);
-        for (int j = 1; j < colunaNaval; j++)
-        {
-            printf("  %d   ", matriz[i][j]);
+         
+    for (int i = 0; i < linha; i++){  
+        for (int j = 0; j < coluna; j++){
+            printf("%d ", matriz[i][j] );
         }
         printf("\n");
-        
-    }
-    
-
+    } 
 }
