8e56033ce382f635e6938a23ddbf90d33e20b875
modestojj <jeanfmodesto6734@gmail.com>
2025-09-18 23:53:45 +0000

    formato losango

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,22 +7,34 @@ int main(){
 int matriz[linha][coluna] = {0};
 int meio = coluna / 2;
 
+ // losango 
+    //cima
+    for(int i  = 0; i < linha / 2 + 1; i++){ //Vai de i = 0 até o meio (linha/2)
+        int inicio = meio - i; //Em cada linha, define a "largura" do losango (inicio até fim).
+        int fim = meio + i;
 
+
+        for(int j = inicio; j <= fim; j++){
+            matriz[i][j] = 3;//Preenche com 3 (seu símbolo escolhido).
+        }
+    }
+
+    //baixo
+    for(int i = linha / 2 + 1; i < linha; i++){ //Vai do meio + (1) até a última linha
+        int distancia = linha - i - 1; //Calcula distancia como a "quantidade de linhas até o fim".
+        int inicio = meio - distancia; //Define inicio e fim espelhados.
+        int fim = meio + distancia;   //Define inicio e fim espelhados.
         
- 
-    //formato X
-     for(int i = 0;i < linha; i++){
-        for(int j = 0; j < coluna; j++){
-            if(i == j || i + j == coluna - 1){//i == j desenha a diagonal principal
-                matriz[i][j] = 3;//i + j == coluna - 1 desenha a diagonal secundária
-            }
+        for(int j = inicio; j <= fim; j++){
+            matriz[i][j] = 3;
         }
     }
 
     for(int i = 0; i < linha; i++){
         for(int j = 0; j < coluna; j++){
-            printf("%d ", matriz[i][j]);
+            printf("%d ", matriz[i][j]); //Imprime a matriz inteira (com 0s onde não tem losango e 3s no losango).
         }
         printf("\n");
-    } 
+        
+    }
 }
